3bd97520b08a37b1a9ac8b63966a3c7b9b14bbd7
TiegoSilva <tiego.silvaa@gmail.com>
2022-09-04 19:45:43 -0300

    levando em consideração numeros errados

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var app = express();
 
 app.get('/', async (req, res) => {
 
+    let printed = "";
 
-    xlsxFile('./Data.xlsx').then( async (rows) => { 
+
+    await xlsxFile('./Data.xlsx').then( async (rows) => { 
 
         for( let x = 0; x < rows.length; x++){
             
-
+            let final = "";
             let  test1 = rows[x][0] + "";
     
             let without_space = await test1.replaceAll(" ", "");
@@ -19,7 +21,7 @@ app.get('/', async (req, res) => {
             let without_parentheses = await without_hyphen.replaceAll('(', "");
             without_parentheses = await without_parentheses.replaceAll(')', ""); 
             let without_plus = await without_parentheses.replaceAll("+", "");
-            let final = "";
+           
 
             if(without_plus.length == 11){
                 final = "55" + without_plus;
@@ -29,19 +31,24 @@ app.get('/', async (req, res) => {
                 final = "55219" + without_plus;
             }else if(without_plus.length == 13){
                 final = without_plus;
+            }else if(without_plus.length == 10){
+                final = without_plus;
             }
 
+            printed += final + "<br />";
             console.log(final);
         }
 
+        
+
     })
 
-    
+    res.send(printed);
 
     //console.log(without_plus);
 
 
-    res.send('tá rodando, rapaziada');
+    
 })
 
 app.listen(process.env.PORT || 3000, () => {
